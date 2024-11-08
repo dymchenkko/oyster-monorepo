5f2f817ae492a9c465e223c99e4b25706def06ce
dymchenkko <elizavetadymchenko20@gmail.com>
2024-11-08 15:43:38 +0100

    test: verify key verification testing

--- attestation/verifier-risczero/host/src/main.rs
+++ attestation/verifier-risczero/host/src/main.rs
@@ -1,7 +1,8 @@
 use clap::Parser;
 use methods::{GUEST_ELF, GUEST_ID};
 use risc0_zkvm::{default_prover, ExecutorEnv, ProverOpts};
-
+use std::fs::File;
+use std::io::Read;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
@@ -19,16 +20,9 @@ fn main() {
         hex::encode(GUEST_ID.map(u32::to_le_bytes).as_flattened())
     );
 
-    let args = Args::parse();
-
-    // Query attestation from the given url
+    let mut attestation_file = File::open("doc").unwrap();
     let mut attestation = Vec::new();
-    ureq::get(&args.url)
-        .call()
-        .unwrap()
-        .into_reader()
-        .read_to_end(&mut attestation)
-        .unwrap();
+    attestation_file.read_to_end(&mut attestation).unwrap();
 
     println!("Attestation size: {}", attestation.len());
 
@@ -40,10 +34,10 @@ fn main() {
     let prover = default_prover();
     // Enable groth16
     let prove_info = prover
-        .prove_with_opts(env, GUEST_ELF, &ProverOpts::groth16())
+        .prove_with_opts(env, GUEST_ELF, &ProverOpts::default())
         .unwrap();
 
     let receipt = prove_info.receipt;
 
-    println!("{:?}", receipt);
+    //println!("{:?}", receipt);
 }
--- attestation/verifier-risczero/methods/guest/src/main.rs
+++ attestation/verifier-risczero/methods/guest/src/main.rs
@@ -15,381 +15,55 @@ use x509_cert::der::Decode;
 // Skips processing certificate extensions and subject/issuers. Verifies only signatures, expiry.
 
 fn main() {
-    // read the attestation
-    let mut attestation = Vec::<u8>::new();
-    env::stdin().read_to_end(&mut attestation).unwrap();
-
-    println!(
-        "Attestation: {} bytes: {:?}",
-        attestation.len(),
-        attestation
-    );
-
-    // assert initial fields
-    assert_eq!(
-        attestation[0..8],
-        [
-            0x84, // the COSE structure is an array of size 4
-            0x44, 0xa1, 0x01, 0x38, 0x22, // protected header, specifying P384 signature
-            0xa0, // empty unprotected header
-            0x59, // payload size of 2 bytes follows
-        ]
-    );
-
-    // get payload size
-    let payload_size = u16::from_be_bytes([attestation[8], attestation[9]]) as usize;
-    println!("Payload size: {payload_size}");
-
-    // assert total size
-    assert_eq!(attestation.len(), 10 + payload_size + 98);
-
-    // payload should be in attestation[10..10 + payload_size]
-    // signature should be in attestation[12 + payload_size..108 + payload_size]
-
-    // skip fields and simply assert length
-    assert_eq!(
-        attestation[10..12],
-        [
-            0xa9, // attestation doc payload is map of size 9
-            // expected keys: module_id, digest, timestamp, pcrs, certificate, cabundle,
-            // public_key, user_data, nonce
-            0x69, // text of size 9, "module_id" key
-        ]
-    );
-    assert_eq!(attestation[21], 0x78); // text with one byte length follows
-
-    // skip to after the module id
-    let mut offset = 23 + attestation[22] as usize;
-
-    assert_eq!(attestation[offset], 0x66); // text of size 6, "digest" key
-    assert_eq!(attestation[offset + 7], 0x66); // text of size 6, "SHA384" value
-
-    // assert timestamp key
-    assert_eq!(attestation[offset + 14], 0x69); // text of size 9
-    assert_eq!(&attestation[offset + 15..offset + 24], b"timestamp");
-    // commit the timestamp value
-    assert_eq!(attestation[offset + 24], 0x1b); // unsigned int, size 8
-    println!("Timestamp: {:?}", &attestation[offset + 25..offset + 33]);
-    env::commit_slice(&attestation[offset + 25..offset + 33]);
-
-    // extract timestamp for expiry checks, convert from milliseconds to seconds
-    let timestamp =
-        u64::from_be_bytes(attestation[offset + 25..offset + 33].try_into().unwrap()) / 1000;
-
-    // assert pcrs key
-    assert_eq!(attestation[offset + 33], 0x64); // text of size 4
-    assert_eq!(&attestation[offset + 34..offset + 38], b"pcrs");
-    // commit pcrs 0, 1 and 2
-    assert_eq!(attestation[offset + 38], 0xb0); // pcrs is a map of size 16
-
-    offset += 39;
-    assert_eq!(
-        attestation[offset..offset + 3],
-        [
-            0x00, // pcr number
-            0x58, // bytes with one byte length follows
-            0x30, // 48 length
-        ]
-    );
-    println!("PCR0: {:?}", &attestation[offset + 3..offset + 51]);
-    env::commit_slice(&attestation[offset + 3..offset + 51]);
-
-    offset += 51;
-    assert_eq!(attestation[offset..offset + 3], [0x01, 0x58, 0x30]);
-    println!("PCR1: {:?}", &attestation[offset + 3..offset + 51]);
-    env::commit_slice(&attestation[offset + 3..offset + 51]);
-
-    offset += 51;
-    assert_eq!(attestation[offset..offset + 3], [0x02, 0x58, 0x30]);
-    println!("PCR2: {:?}", &attestation[offset + 3..offset + 51]);
-    env::commit_slice(&attestation[offset + 3..offset + 51]);
-
-    // skip rest of the pcrs, 3 to 15
-    offset += 51;
-    assert_eq!(attestation[offset..offset + 3], [0x03, 0x58, 0x30]);
-    offset += 51;
-    assert_eq!(attestation[offset..offset + 3], [0x04, 0x58, 0x30]);
-    offset += 51;
-    assert_eq!(attestation[offset..offset + 3], [0x05, 0x58, 0x30]);
-    offset += 51;
-    assert_eq!(attestation[offset..offset + 3], [0x06, 0x58, 0x30]);
-    offset += 51;
-    assert_eq!(attestation[offset..offset + 3], [0x07, 0x58, 0x30]);
-    offset += 51;
-    assert_eq!(attestation[offset..offset + 3], [0x08, 0x58, 0x30]);
-    offset += 51;
-    assert_eq!(attestation[offset..offset + 3], [0x09, 0x58, 0x30]);
-    offset += 51;
-    assert_eq!(attestation[offset..offset + 3], [0x0a, 0x58, 0x30]);
-    offset += 51;
-    assert_eq!(attestation[offset..offset + 3], [0x0b, 0x58, 0x30]);
-    offset += 51;
-    assert_eq!(attestation[offset..offset + 3], [0x0c, 0x58, 0x30]);
-    offset += 51;
-    assert_eq!(attestation[offset..offset + 3], [0x0d, 0x58, 0x30]);
-    offset += 51;
-    assert_eq!(attestation[offset..offset + 3], [0x0e, 0x58, 0x30]);
-    offset += 51;
-    assert_eq!(attestation[offset..offset + 3], [0x0f, 0x58, 0x30]);
-    offset += 51;
-    println!("Skipped rest of the pcrs");
-
-    // assert certificate key
-    assert_eq!(attestation[offset], 0x6b); // text of size 11
-    assert_eq!(&attestation[offset + 1..offset + 12], b"certificate");
-    // get leaf certificate
-    assert_eq!(attestation[offset + 12], 0x59); // bytes where two byte length follows
-    let leaf_cert_size =
-        u16::from_be_bytes([attestation[offset + 13], attestation[offset + 14]]) as usize;
-    let leaf_cert_offset = offset + 15;
-    let leaf_cert =
-        x509_cert::Certificate::from_der(&attestation[offset + 15..offset + 15 + leaf_cert_size])
-            .unwrap();
-    offset += 15 + leaf_cert_size;
-
-    // assert cabundle key
-    assert_eq!(attestation[offset], 0x68); // text of length 8
-    assert_eq!(&attestation[offset + 1..offset + 9], b"cabundle");
-
-    // cabundle should be an array, read length
-    let chain_size = attestation[offset + 9];
-    // just restrict chain size instead of figuring out parsing too much
-    // works for tiny field encoded cabundle up to 16 length
-    assert!(chain_size > 0x80 && chain_size <= 0x90);
-    let chain_size = chain_size - 0x80; // real size is minus 0x80 for bytes type
-
-    // verify certificate chain
-    // first certificate in the list is the root certificate
-    // last certificate in the list signs the leaf certificate obtained above
-
-    // track start of each section so we know where to proceed from after the block
-    offset = offset + 10;
-    {
-        // start with the root cert
-
-        // parse root cert
-        assert_eq!(attestation[offset], 0x59); // bytes where two byte length follows
-        let size = u16::from_be_bytes([attestation[offset + 1], attestation[offset + 2]]) as usize;
-
-        // cert with the pubkey, start with the root
-        let mut parent_cert =
-            x509_cert::Certificate::from_der(&attestation[offset + 3..offset + 3 + size]).unwrap();
-        // assert parent cert expiry
-        assert!(
-            parent_cert
-                .tbs_certificate
-                .validity
-                .not_before
-                .to_unix_duration()
-                .as_secs()
-                < timestamp
-        );
-        assert!(
-            parent_cert
-                .tbs_certificate
-                .validity
-                .not_after
-                .to_unix_duration()
-                .as_secs()
-                > timestamp
-        );
-
-        // commit the root pubkey
-        let pubkey = parent_cert
-            .tbs_certificate
-            .subject_public_key_info
-            .subject_public_key
-            .raw_bytes();
-        println!(
-            "Root certificate public key: {} bytes: {:?}",
-            pubkey.len(),
-            pubkey
-        );
-        // assert that the pubkey size is 97 in case it changes later
-        assert_eq!(pubkey.len(), 97);
-        assert_eq!(pubkey[0], 0x04);
-        env::commit_slice(&pubkey[1..]);
-
-        // start of next cert that is to be verified
-        offset = offset + 3 + size;
-
-        for _ in 0..chain_size - 1 {
-            // parse child cert
-            assert_eq!(attestation[offset], 0x59); // bytes where two byte length follows
-            let size =
-                u16::from_be_bytes([attestation[offset + 1], attestation[offset + 2]]) as usize;
-
-            // parse the next cert and get the public key
-            let child_cert =
-                x509_cert::Certificate::from_der(&attestation[offset + 3..offset + 3 + size])
-                    .unwrap();
-            // assert cert expiry
-            assert!(
-                child_cert
-                    .tbs_certificate
-                    .validity
-                    .not_before
-                    .to_unix_duration()
-                    .as_secs()
-                    < timestamp
-            );
-            assert!(
-                child_cert
-                    .tbs_certificate
-                    .validity
-                    .not_after
-                    .to_unix_duration()
-                    .as_secs()
-                    > timestamp
-            );
-
-            // verify signature
-            // the tbs cert is already available in DER form in the attestation, use that
-            assert_eq!(attestation[offset + 3], 0x30); // ASN.1 Sequence
-            assert_eq!(attestation[offset + 4], 0x82); // two byte length follows
-            assert_eq!(attestation[offset + 7], 0x30); // ASN.1 Sequence
-            assert_eq!(attestation[offset + 8], 0x82); // two byte length follows
-            let cert_size =
-                u16::from_be_bytes([attestation[offset + 9], attestation[offset + 10]]) as usize;
-            let msg = &attestation[offset + 7..offset + 11 + cert_size];
-            let sig = Signature::from_der(&child_cert.signature.raw_bytes()).unwrap();
-            let pubkey = parent_cert
-                .tbs_certificate
-                .subject_public_key_info
-                .subject_public_key
-                .raw_bytes();
-            let vkey = VerifyingKey::from_sec1_bytes(&pubkey).unwrap();
-            vkey.verify(&msg, &sig).unwrap();
-
-            // set up for next iteration
-            parent_cert = child_cert;
-            offset = offset + 3 + size;
-        }
-
-        // assert leaf cert expiry
-        assert!(
-            leaf_cert
-                .tbs_certificate
-                .validity
-                .not_before
-                .to_unix_duration()
-                .as_secs()
-                < timestamp
-        );
-        assert!(
-            leaf_cert
-                .tbs_certificate
-                .validity
-                .not_after
-                .to_unix_duration()
-                .as_secs()
-                > timestamp
-        );
-
-        // verify the leaf cert with the last cert in the chain
-        // the tbs cert is already available in DER form in the attestation, use that
-        assert_eq!(attestation[leaf_cert_offset], 0x30); // ASN.1 Sequence
-        assert_eq!(attestation[leaf_cert_offset + 1], 0x82); // two byte length follows
-        assert_eq!(attestation[leaf_cert_offset + 4], 0x30); // ASN.1 Sequence
-        assert_eq!(attestation[leaf_cert_offset + 5], 0x82); // two byte length follows
-        let cert_size = u16::from_be_bytes([
-            attestation[leaf_cert_offset + 6],
-            attestation[leaf_cert_offset + 7],
-        ]) as usize;
-        let msg = &attestation[leaf_cert_offset + 4..leaf_cert_offset + 8 + cert_size];
-        let sig = Signature::from_der(&leaf_cert.signature.raw_bytes()).unwrap();
-        let pubkey = parent_cert
-            .tbs_certificate
-            .subject_public_key_info
-            .subject_public_key
-            .raw_bytes();
-        let vkey = VerifyingKey::from_sec1_bytes(&pubkey).unwrap();
-        vkey.verify(&msg, &sig).unwrap();
-    }
-
-    // assert public_key key
-    assert_eq!(attestation[offset], 0x6a); // text of size 10
-    assert_eq!(&attestation[offset + 1..offset + 11], b"public_key");
-    // commit public key, expected length of 64 since it is a secp256k1 key
-    assert_eq!(attestation[offset + 11], 0x58); // bytes where one byte length follows
-
-    // get public key length
-    let pubkey_len = attestation[offset + 12] as usize;
-    println!(
-        "Public key: {pubkey_len} bytes: {:?}",
-        &attestation[offset + 13..offset + 13 + pubkey_len]
-    );
-    env::commit_slice(&[attestation[offset + 12]]);
-    env::commit_slice(&attestation[offset + 13..offset + 13 + pubkey_len]);
-
-    offset = offset + 13 + pubkey_len;
-
-    // assert user_data key
-    assert_eq!(attestation[offset], 0x69); // text of size 9
-    assert_eq!(&attestation[offset + 1..offset + 10], b"user_data");
-    // commit user data
-    // handle cases up to 65536 size
-    let (user_data_size, user_data) = if attestation[offset + 10] == 0xf6 {
-        // empty
-        (0, [].as_slice())
-    } else if attestation[offset + 10] == 0x58 {
-        // one byte length follows
-        let size = attestation[offset + 11] as u16;
-
-        (size, &attestation[offset + 12..offset + 12 + size as usize])
-    } else {
-        // only allow 2 byte lengths as max
-        // technically, this is already enforced by COSE doc size parsing
-        assert_eq!(attestation[offset + 10], 0x59);
-
-        let size = u16::from_be_bytes([attestation[offset + 11], attestation[offset + 12]]);
-
-        (size, &attestation[offset + 13..offset + 13 + size as usize])
-    };
-    println!("User data: {} bytes: {:?}", user_data_size, user_data);
-    // commit 2 byte length, then data
-    env::commit_slice(&user_data_size.to_be_bytes());
-    env::commit_slice(user_data);
-
-    // prepare COSE verification hash
-    let mut hasher = sha2::Sha384::new();
-    // array with 4 elements
-    hasher.update(&[0x84]);
-    // context field length
-    hasher.update(&[0x6a]);
-    // context field
-    hasher.update("Signature1");
-    // body_protected
-    hasher.update(&[0x44, 0xa1, 0x01, 0x38, 0x22]);
-    // empty aad
-    hasher.update(&[0x40]);
-    // payload length
-    hasher.update(&[0x59, attestation[8], attestation[9]]);
-    // payload
-    hasher.update(&attestation[10..10 + payload_size]);
-    let hash = hasher.finalize();
-
-    // verify signature
-    // signature size
-    assert_eq!(attestation[payload_size + 10], 0x58); // bytes where one byte length follows
-    assert_eq!(attestation[payload_size + 11], 0x60); // 96 length
-
-    let leaf_cert_pubkey = leaf_cert
-        .tbs_certificate
-        .subject_public_key_info
-        .subject_public_key
-        .raw_bytes()
-        .to_owned();
-    let verifying_key = VerifyingKey::from_sec1_bytes(&leaf_cert_pubkey).unwrap();
-    let r: [u8; 48] = attestation[12 + payload_size..60 + payload_size]
-        .try_into()
-        .unwrap();
-    let s: [u8; 48] = attestation[60 + payload_size..108 + payload_size]
-        .try_into()
-        .unwrap();
-    let signature = Signature::from_scalars(r, s).unwrap();
-
-    verifying_key.verify_prehash(&hash, &signature).unwrap();
-
-    println!("Done!");
+    let raw_signature = [
+        48, 101, 2, 48, 6, 79, 12, 11, 17, 224, 42, 37, 140, 155, 53, 160, 253, 255, 110, 205, 250,
+        7, 234, 222, 236, 129, 42, 34, 26, 114, 39, 205, 87, 76, 70, 80, 74, 7, 218, 100, 255, 7,
+        234, 237, 21, 170, 223, 117, 225, 9, 177, 208, 2, 49, 0, 168, 102, 80, 226, 70, 3, 107,
+        201, 220, 73, 38, 36, 18, 166, 81, 212, 61, 47, 19, 177, 166, 46, 14, 26, 201, 151, 245,
+        149, 154, 144, 248, 127, 81, 39, 136, 250, 5, 141, 239, 59, 161, 30, 184, 25, 137, 162,
+        206, 215,
+    ];
+    let sig = Signature::from_der(&raw_signature).unwrap();
+    let vkey = VerifyingKey::from_sec1_bytes(&[
+        4, 152, 125, 172, 31, 121, 146, 192, 17, 174, 103, 215, 179, 247, 29, 235, 146, 74, 172,
+        229, 98, 163, 253, 105, 133, 119, 125, 87, 12, 17, 21, 165, 102, 165, 74, 58, 249, 101, 0,
+        165, 89, 124, 90, 61, 137, 27, 153, 153, 242, 123, 167, 182, 132, 111, 109, 185, 189, 248,
+        252, 211, 70, 94, 10, 70, 174, 74, 202, 40, 185, 45, 156, 231, 75, 167, 232, 173, 143, 145,
+        91, 124, 71, 103, 41, 17, 97, 59, 215, 193, 164, 121, 6, 217, 88, 254, 193, 157, 21,
+    ])
+    .unwrap();
+    vkey.verify(
+        &[
+            48, 130, 2, 69, 160, 3, 2, 1, 2, 2, 21, 0, 153, 184, 9, 212, 122, 23, 24, 182, 27, 155,
+            147, 88, 106, 51, 141, 188, 77, 128, 244, 133, 48, 10, 6, 8, 42, 134, 72, 206, 61, 4,
+            3, 3, 48, 129, 137, 49, 60, 48, 58, 6, 3, 85, 4, 3, 12, 51, 57, 52, 49, 51, 101, 53,
+            52, 55, 99, 52, 101, 56, 100, 55, 55, 49, 46, 122, 111, 110, 97, 108, 46, 101, 117, 45,
+            119, 101, 115, 116, 45, 49, 46, 97, 119, 115, 46, 110, 105, 116, 114, 111, 45, 101,
+            110, 99, 108, 97, 118, 101, 115, 49, 12, 48, 10, 6, 3, 85, 4, 11, 12, 3, 65, 87, 83,
+            49, 15, 48, 13, 6, 3, 85, 4, 10, 12, 6, 65, 109, 97, 122, 111, 110, 49, 11, 48, 9, 6,
+            3, 85, 4, 6, 19, 2, 85, 83, 49, 11, 48, 9, 6, 3, 85, 4, 8, 12, 2, 87, 65, 49, 16, 48,
+            14, 6, 3, 85, 4, 7, 12, 7, 83, 101, 97, 116, 116, 108, 101, 48, 30, 23, 13, 50, 52, 49,
+            49, 48, 56, 48, 55, 50, 51, 49, 51, 90, 23, 13, 50, 52, 49, 49, 48, 57, 48, 55, 50, 51,
+            49, 51, 90, 48, 129, 142, 49, 11, 48, 9, 6, 3, 85, 4, 6, 19, 2, 85, 83, 49, 19, 48, 17,
+            6, 3, 85, 4, 8, 12, 10, 87, 97, 115, 104, 105, 110, 103, 116, 111, 110, 49, 16, 48, 14,
+            6, 3, 85, 4, 7, 12, 7, 83, 101, 97, 116, 116, 108, 101, 49, 15, 48, 13, 6, 3, 85, 4,
+            10, 12, 6, 65, 109, 97, 122, 111, 110, 49, 12, 48, 10, 6, 3, 85, 4, 11, 12, 3, 65, 87,
+            83, 49, 57, 48, 55, 6, 3, 85, 4, 3, 12, 48, 105, 45, 48, 52, 50, 100, 55, 55, 102, 56,
+            97, 100, 98, 100, 50, 53, 51, 54, 57, 46, 101, 117, 45, 119, 101, 115, 116, 45, 49, 46,
+            97, 119, 115, 46, 110, 105, 116, 114, 111, 45, 101, 110, 99, 108, 97, 118, 101, 115,
+            48, 118, 48, 16, 6, 7, 42, 134, 72, 206, 61, 2, 1, 6, 5, 43, 129, 4, 0, 34, 3, 98, 0,
+            4, 51, 51, 98, 183, 137, 224, 105, 99, 133, 124, 223, 109, 113, 20, 75, 93, 244, 1,
+            224, 175, 12, 239, 77, 77, 114, 31, 105, 165, 73, 45, 182, 165, 235, 128, 107, 226, 91,
+            61, 125, 154, 4, 150, 125, 166, 163, 151, 207, 153, 222, 139, 66, 59, 11, 79, 9, 117,
+            247, 178, 161, 73, 15, 120, 170, 162, 112, 118, 254, 29, 199, 188, 155, 225, 21, 235,
+            181, 19, 78, 170, 39, 80, 32, 190, 168, 78, 243, 93, 29, 181, 23, 94, 60, 123, 57, 70,
+            252, 62, 163, 102, 48, 100, 48, 18, 6, 3, 85, 29, 19, 1, 1, 255, 4, 8, 48, 6, 1, 1,
+            255, 2, 1, 0, 48, 14, 6, 3, 85, 29, 15, 1, 1, 255, 4, 4, 3, 2, 2, 4, 48, 29, 6, 3, 85,
+            29, 14, 4, 22, 4, 20, 42, 139, 196, 103, 225, 60, 172, 39, 203, 107, 201, 114, 76, 74,
+            45, 179, 201, 86, 158, 98, 48, 31, 6, 3, 85, 29, 35, 4, 24, 48, 22, 128, 20, 246, 212,
+            48, 121, 113, 73, 132, 54, 237, 40, 72, 148, 50, 144, 138, 250, 5, 123, 253, 73,
+        ],
+        &sig,
+    )
+    .unwrap();
 }
